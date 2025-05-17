99d6240e9a2363b56944dd6423363995498ddb7f
Ian Shehadeh <ian@shehadeh.net>
2025-05-17 09:40:59 -0400

    polyomino placement

--- blokus-demo/src/lib/Blokus.svelte
+++ blokus-demo/src/lib/Blokus.svelte
@@ -1,34 +1,63 @@
 <div id='board' style="--board-width: {board.width}; --board-height: {board.height}">
-    {#each cells as state}
-        <Cell state={state} />
+    {#each cells as state, i}
+        <div class="cell {state}" onmousedown={(e) => onMouseDown(e, i)} onmouseenter={(e) => onMouseEnter(e, i)}>
+        </div>
     {/each}
 </div>
 
 <script lang="ts">
     import { Blokus, TileState } from '../../../cgtjs/dist/index.js';
-	import Cell from './Cell.svelte';
 
     const {
         width,
-        height
+        height,
+        userPolyomino,
     }: {
         width: number|bigint,
-        height: number|bigint
+        height: number|bigint,
+        userPolyomino: Blokus|undefined,
     } = $props();
 
     const board = Blokus.empty(BigInt(width), BigInt(height));
+    let previewBoard = Blokus.empty(BigInt(width), BigInt(height));
     const cells = $state(new Array(Number(board.width * board.height)).fill(TileState.Empty));
+    let hoveredCell = $state({ x: 0n, y: 0n});
     updateCellsArray();
 
     function updateCellsArray() {
         for (const cellIndex in cells) {
-            const currentState = board.getByIndex(BigInt(cellIndex));
+            const currentState = previewBoard.getByIndex(BigInt(cellIndex));
             const oldState = cells[cellIndex];
             if (oldState !== currentState) {
                 cells[cellIndex] = currentState;
             }
         }
     }
+
+    $effect(() => {
+        if (userPolyomino) {
+            previewBoard = board.clone();
+            previewBoard.tryPlacePolyomino(hoveredCell.x, hoveredCell.y, userPolyomino, userPolyomino.width/2n, userPolyomino.height/2n);
+            updateCellsArray();
+        }
+    })
+
+    function onMouseDown(event: MouseEvent, cellIndex: number) {
+        hoveredCell.x = BigInt(cellIndex) % previewBoard.width;
+        hoveredCell.y = BigInt(cellIndex) / previewBoard.width;
+        if (userPolyomino) {
+            board.tryPlacePolyomino(hoveredCell.x, hoveredCell.y, userPolyomino, userPolyomino.width/2n, userPolyomino.height/2n);
+            previewBoard = board.clone();
+
+            updateCellsArray();
+        }
+    }
+
+    function onMouseEnter(event: MouseEvent, cellIndex: number) {
+        hoveredCell.x = BigInt(cellIndex) % previewBoard.width;
+        hoveredCell.y = BigInt(cellIndex) / previewBoard.width;
+    }
+
 </script>
 
 <style>
@@ -37,5 +66,24 @@
         gap: 4px;
         grid-template-rows: repeat(var(--board-height), 1rem);
         grid-template-columns: repeat(var(--board-width), 1rem);
+    }
+
+    .cell {
+        background-color: var(--cell-color);
+    }
+
+    .interior {
+        --cell-color: blue;
+    }
+    .corner {
+        --cell-color: green;
+    }
+
+    .side {
+        --cell-color: orange;
+    }
+
+    .empty {
+        --cell-color: black;
     }
 </style>
--- blokus-demo/src/routes/+page.svelte
+++ blokus-demo/src/routes/+page.svelte
@@ -1,8 +1,17 @@
 <script>
 	import Blokus from "$lib/Blokus.svelte";
-
+	import { Blokus as BlokusBitmap } from "../../../cgtjs/dist";
 </script>
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
-<Blokus width={10} height={10}></Blokus>
+<Blokus
+	width={10}
+	height={10}
+	userPolyomino={
+		BlokusBitmap.fromString(`
+			cssc
+			siis
+			sis
+			csc`)
+	}></Blokus>
